7c3413507c7d686785239b1d6f2cad2756455914
KBalazs52 <kedvesbalazs2010@gmail.com>
2026-05-11 08:46:46 +0200

    Jelzolampa alja + hatter

--- KozosRajz.py
+++ KozosRajz.py
@@ -1,3 +1,28 @@
 #jelzolampa teste, hatterszin K. Balázs
 
 #jelzolampa felso resze es fenyek H. Bálint
+
+#alap beallitasok
+import turtle
+turtle.speed(0)
+turtle.color("Gray")
+#pozicio
+turtle.penup()
+turtle.forward(25)
+turtle.pendown()
+#hatter szine
+turtle.bgcolor("DarkSlateGray1")
+#jelzolampa szine
+turtle.fillcolor("Gray")
+turtle.begin_fill()
+#jelzolampa alja
+i=0
+while i<2:
+    turtle.right(90)
+    turtle.forward(200)
+    turtle.right(90)
+    turtle.forward(50)
+    i+=1
+
+turtle.end_fill()
+turtle.done()
